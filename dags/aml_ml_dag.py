e62ca64fa242434d40fd597bb7c02ad72a227ced
Meuracha <uracharittikulsittichai@gmail.com>
2026-05-25 23:00:17 +0700

    fix: remove unused numpy import in register_model

--- dags/aml_ml_dag.py
+++ dags/aml_ml_dag.py
@@ -590,7 +590,6 @@ def register_model(**context):
     sys.path.insert(0, "/opt/airflow/dags")
     import mlflow
     import mlflow.xgboost
-    import numpy as np
     import pandas as pd
     import pyarrow as pa
     import pyarrow.parquet as pq
